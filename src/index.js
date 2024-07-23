aa4b6a1d314773940b7537ba41dacb3adb3dd78b
britniangela <66388266+britniangela@users.noreply.github.com>
2024-07-23 12:23:40 -0400

    updated time zone js/updated css

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
 function updateTime() {
   let aucklandElement = document.querySelector("#auckland");
-  let aucklandDateElement = aucklandElement.querySelector(".date");
-  let aucklandTimeElement = aucklandElement.querySelector(".time");
-  let aucklandTimeZone = moment().tz("Pacific/Auckland");
+  if (aucklandElement) {
+    let aucklandDateElement = aucklandElement.querySelector(".date");
+    let aucklandTimeElement = aucklandElement.querySelector(".time");
+    let aucklandTimeZone = moment().tz("Pacific/Auckland");
 
-  aucklandDateElement.innerHTML = aucklandTimeZone.format("MMMM Do, YYYY");
-  aucklandTimeElement.innerHTML = aucklandTimeZone.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    aucklandDateElement.innerHTML = aucklandTimeZone.format("MMMM Do, YYYY");
+    aucklandTimeElement.innerHTML = aucklandTimeZone.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let bangkokElement = document.querySelector("#bangkok");
-  let bangkokDateElement = bangkokElement.querySelector(".date");
-  let bangkokTimeElement = bangkokElement.querySelector(".time");
-  let bangkokTimeZone = moment().tz("Asia/Bangkok");
+  if (bangkokElement) {
+    let bangkokDateElement = bangkokElement.querySelector(".date");
+    let bangkokTimeElement = bangkokElement.querySelector(".time");
+    let bangkokTimeZone = moment().tz("Asia/Bangkok");
 
-  bangkokDateElement.innerHTML = bangkokTimeZone.format("MMMM Do, YYYY");
-  bangkokTimeElement.innerHTML = bangkokTimeZone.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    bangkokDateElement.innerHTML = bangkokTimeZone.format("MMMM Do, YYYY");
+    bangkokTimeElement.innerHTML = bangkokTimeZone.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
    <div class="city-container">
           <div class="city">
-            <h2>${cityTimeZone}</h2>
+            <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
           </div>
           <div class="time">${cityTime.format(
